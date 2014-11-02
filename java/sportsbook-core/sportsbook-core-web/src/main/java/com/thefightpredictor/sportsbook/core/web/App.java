b15bac59713da8d75e7ffedd620bbca68bc003fd
Austin Partridge <austin.p@outlook.com>
2014-11-01 22:35:22 -0400

    Closer to a working compiling project

--- java/sportsbook-core/sportsbook-core-web/src/main/java/com/thefightpredictor/sportsbook/core/web/App.java
+++ java/sportsbook-core/sportsbook-core-web/src/main/java/com/thefightpredictor/sportsbook/core/web/App.java
@@ -1,11 +1,14 @@
 package com.thefightpredictor.sportsbook.core.web;
 
+import javax.ws.rs.Path;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	@Path("blah")
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
