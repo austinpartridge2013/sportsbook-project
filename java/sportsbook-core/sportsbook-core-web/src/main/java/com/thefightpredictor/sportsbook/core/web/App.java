3eb919900cfddb188feebdd032ad365f362b0148
Austin <austin.p@outlook.com>
2015-02-07 20:00:19 -0500

    Adding beans xml

--- java/sportsbook-core/sportsbook-core-web/src/main/java/com/thefightpredictor/sportsbook/core/web/App.java
+++ java/sportsbook-core/sportsbook-core-web/src/main/java/com/thefightpredictor/sportsbook/core/web/App.java
@@ -5,13 +5,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-import com.thefightpredictor.sportsbook.core.service.HelloWorldEjb;
+import com.thefightpredictor.sportsbook.core.service.interfaces.IHelloWorldEjb;
 
 @Path("blah")
 public class App
 {
     @Inject
-    private HelloWorldEjb helloWorldEjb;
+    private IHelloWorldEjb helloWorldEjb;
 
     @Path("blah")
     @GET
