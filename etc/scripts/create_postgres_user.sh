aef05f4c7fc3a4479975d0a9e08258af232a0994
Austin Partridge <austin.p@outlook.com>
2015-02-08 18:55:49 -0500

    Fixing database stuff

--- etc/scripts/create_postgres_user.sh
+++ etc/scripts/create_postgres_user.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 psql -U postgres -c "CREATE USER sportsbook WITH PASSWORD 'fightpredictor'"
+psql -U postgres -c "DROP SCHEMA IF EXISTS sportsbook"
+psql -U postgres -c "CREATE SCHEMA sportsbook"
+psql -U postgres -c "ALTER USER sportsbook SET search_path TO public"
