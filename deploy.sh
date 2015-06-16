f916fec0c9266e3b551583896ccd535ceb7be15b
Austin Partridge <austin.p@outlook.com>
2015-06-15 20:06:31 -0400

    Updating reference to easybooks ca

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm $JBOSS_HOME/standalone/deployments/*.ear
+rm -rf $JBOSS_HOME/standalone/deployments/*.ear
 cp java/easybooks-core/easybooks-core-ear/target/*.ear $JBOSS_HOME/standalone/deployments
