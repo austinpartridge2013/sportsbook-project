3a47e67486f380a55adf8df453eb0c2193064386
Austin <austin.p@outlook.com>
2015-06-16 22:11:20 -0400

    Adding logging capabilities

--- java/easybooks-core/easybooks-trivial-fileops/src/main/java/ca/easybooks/fileops/trivial/TrivialFileOperations.java
+++ java/easybooks-core/easybooks-trivial-fileops/src/main/java/ca/easybooks/fileops/trivial/TrivialFileOperations.java
@@ -6,21 +6,28 @@ import java.io.IOException;
 import javax.ejb.Stateless;
 
 import org.apache.commons.io.FileUtils;
+import org.jboss.logging.Logger;
 
 import ca.easybooks.data.transferobject.LedgerEntryInput;
 import ca.easybooks.service.interfaces.FileOperations;
 
 @Stateless
 public class TrivialFileOperations implements FileOperations {
+    private static final Logger log = Logger.getLogger(TrivialFileOperations.class);
+
     private static final String DEFAULT_FILE_PATH = "/tmp/";
 
     public void saveFile(final LedgerEntryInput ledgerInput) {
         final String fileName =
                 DEFAULT_FILE_PATH +
                 ledgerInput.getFileName() == null ? "Unknown" : ledgerInput.getFileName();
+
+        log.debug("Writing local file to " + fileName);
+
         try {
             FileUtils.writeByteArrayToFile(new File(fileName), ledgerInput.getFileData());
         } catch (final IOException e) {
+            log.error("Error storing file", e);
             throw new RuntimeException("Unable to store file", e);
         }
     }
